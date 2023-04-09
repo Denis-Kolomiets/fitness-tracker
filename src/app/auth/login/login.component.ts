09ab63e92da7c0675d0949fa853600a7c4657845
Denis Kolomiets <deniskolomiets988@gmail.com>
2023-04-09 22:31:36 +0300

    sidenav and toolbar

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
@@ -6,7 +6,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginGroupForm = new FormGroup({
     login: new FormControl('',[Validators.required, Validators.email]),
     password: new FormControl('',[Validators.required])
