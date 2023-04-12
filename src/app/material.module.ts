775f9cc4e22e607f04aa666367739b81b47f6b54
Denis Kolomiets <deniskolomiets988@gmail.com>
2023-04-12 23:34:34 +0300

    update app

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,12 @@ import { MatNativeDateModule } from "@angular/material/core"
 import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatListModule } from '@angular/material/list'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatCardModule } from '@angular/material/card'
+import { MatSelectModule } from '@angular/material/select'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+
 @NgModule({
     imports: [
         MatButtonModule,
@@ -18,7 +24,12 @@ import { MatToolbarModule } from '@angular/material/toolbar'
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule
     ],
     exports: [
         MatButtonModule,
@@ -29,7 +40,12 @@ import { MatToolbarModule } from '@angular/material/toolbar'
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatSelectModule,
+        MatProgressSpinnerModule
     ]
 })
 export class MaterialModule {
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.scss']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
+  progress = 10
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+  onStop(){
+  }
 
 }
